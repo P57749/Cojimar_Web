93501925ef6d5cbf3f850d468a16dcf04eae5a26
Pepe <polopepe567@gmail.com>
2023-05-09 15:48:13 -0400

    version 0.1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet"  type="text/css" href="/style.php" />
+        <title>Elportaldecojimar</title>
+        </head>
+        <body>
+          <header class="container-principal">
+            <div class="cover1 d-flex justify-content-center align-items-center">
+              
+              <a class="nav-link" href="#">Features</a>
+              
+              <a class="navbar-brand" href="#">El Portal de Cojimar </a>
+              
+              <a class="nav-link" href="#">Pricing</a>
+            </div>
+            <div class="cover2 d-flex justify-content-center align-items-center flex-column">
+              <h1>
+                lorepsum
+                </h1>
+                <p>
+                  la papa
+                </p>
+                <button class="btn btn-darken">
+                  conoce más 
+                  </button>
+            </div>
+          </header>
+          <section>
+
+          </section>
+
+        </body>
+
+</html>
---
+++ style.php
@@ -0,0 +1,35 @@
+.container - principal {
+background-image: url(./macarrones.jpeg);
+height: 100vh;
+background-position: center;
+background-size: cover;
+background-color: rgba(0, 0, 0, 0.2);
+background-blend-mode: darken;
+overflow: hidden;
+}
+.navbar {
+position: relative;
+display: -ms-flexbox;
+display: flex;
+-ms-flex-wrap: wrap;
+flex-wrap: wrap;
+-ms-flex-align: center;
+align-items: center;
+-ms-flex-pack: justify;
+justify-content: space-between;
+padding: 0.5rem 1rem;
+}
+header a{
+font-family:Arial, Helvetica, sans-serif;
+align-items: center;
+color: aliceblue;
+
+}
+
+.cover2{
+
+margin-top: 8.5cm;
+margin-right: 2cm;
+padding-inline-start: 2cm;
+color:aliceblue;
+}
